47e31c52eed9a62dc06ef69a6297afdb54b1bc6b
shubham <sk37428@gmail.com>
2026-01-17 19:06:59 +0530

    gateway implemented for ai service

--- activity-service/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activity-service/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,5 +1,6 @@
 package com.fitness.activityservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
